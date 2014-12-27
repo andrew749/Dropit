ed9dacbcb07e0292d2266c9cf804e7e31ff4038d
Andrew Codispoti <andrewcodispoti@Andrews-Mac-mini.local>
2014-12-27 12:51:51 -0500

    Initial Commit

---
+++ Dropit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dropit
+//
+//  Created by Andrew Codispoti on 2014-12-27.
+//  Copyright (c) 2014 Andrew Codispoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dropit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dropit
+//
+//  Created by Andrew Codispoti on 2014-12-27.
+//  Copyright (c) 2014 Andrew Codispoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
